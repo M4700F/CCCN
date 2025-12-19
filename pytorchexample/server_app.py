7c8e5b113fcb11f152beca4cccda8ae2fb241250
Maroof Ahmed <96487710+M4700F@users.noreply.github.com>
2025-12-19 19:55:29 +0600

    Change the csv file name

--- pytorchexample/server_app.py
+++ pytorchexample/server_app.py
@@ -62,7 +62,7 @@ def global_evaluate(server_round: int, arrays: ArrayRecord) -> MetricRecord:
     else:
         mode = 'a'
 
-    with open("baseline_no_attack_vanilla_non_iid_results.csv", mode) as f:
+    with open("delayed_free_rider_attack_with_running_avg_non_iid_results.csv", mode) as f:
         if server_round == 1:
             f.write("round,loss,accuracy\n")
         f.write(f"{server_round},{test_loss},{test_acc}\n")
